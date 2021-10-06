3b3b628f6d22d202005b1dc05c9e99c6e9a4ce3e
Velis <velison.vardanyan@gmail.com>
2021-10-06 16:02:47 +0400

    fdf

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -9,6 +9,11 @@ public class App extends Jooby {
     get("/", ctx -> "Welcome to Jooby!");
   }
 
+  {
+
+
+  }
+
   public static void main(final String[] args) {
     runApp(args, App::new);
   }
