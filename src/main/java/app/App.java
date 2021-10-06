e1f2fb0811abb1d09074d22c1ffa88dc3cc99415
Vardanyan <velison.vardanyan@teamsolutions.am>
2021-10-06 16:24:35 +0400

    rollback

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -9,11 +9,6 @@ public class App extends Jooby {
     get("/", ctx -> "Welcome to Jooby!");
   }
 
-  {
-
-
-  }
-
   public static void main(final String[] args) {
     runApp(args, App::new);
   }
